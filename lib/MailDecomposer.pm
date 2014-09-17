4d8162e0e15f9244a851b555d64ce2b21e2344ba
Stephen Wan <stephenwan@udomain.com.hk>
2014-09-17 18:51:39 +0800

    Development suspended.

--- lib/MailDecomposer.pm
+++ lib/MailDecomposer.pm
@@ -3,19 +3,136 @@ package MailDecomposer;
 use strict; 
 use warnings;
 
-use MIME::Words;
+use File::Spec;
+use MIME::Words qw /:all/;
 use MIME::Parser;
 use MIME::Entity;
-
+use Email::Address;
+use Data::Dumper;
 use Carp;
+use Log::Log4perl qw(:easy);
 use Exporter;
 
 our @ISA = qw(Exporter);
+our @EXPORT_OK = qw();
 
 
+
+sub decompose {
+    my $target = shift;
+    my $meta_options = shift;    
+    my $data_options = shift // {};
+
+    my $target_file = $target->{file};
+    my $meta_dir = $meta->{dir};
+        
+    DEBUG('Decompose $target_file');    
+    unless (-e $target_flie && -f $target_flie && -r $target_flie) {
+        ERROR('Decompose failed: $target_file does not exist or not readable.');
+        return undef;        
+    }
+            
+    my ($volume,$directories,$file) = File::Spec->splitpath( $target_file );    
+    my ($basename, $ext) = split /[.]/, $file;
+
+    unless ($basename) {
+        ERROR('Empty target file basename.');
+        return undef;        
+    }
+    WARN('Target file does not end with eml.') unless $ext eq 'eml';
+    
+    my $decomposed_to_dir = $data_options->{dir} //
+        File::Spec->catdir($directories, $basename.'.decomposed')                            
+    DEBUG('Decompose into $decomposed_to_dir');
+
+    my $decomposed_msg_prefix = $data_options->{msg_prefix} // 'msg-decomposed-';        
+    DEBUG('Decompose using msg-prefix $decomposed_msg_prefix');
+        
+    $parser = MIME::Parser->new;
+    $parser->output_under($decomposed_to_dir);
+    $parser->filer->output_prefix($decomposed_msg_prefix);
+
+    my $_entity = undef;
+    eval { $_entity = $parser->parse_open($target_flie) };
+    
+    if ($@) {
+        $results  = $parser->results;
+        $decapitated = $parser->last_head;  ### get last top-level head
+        ERROR("Parse error: ".$@);        
+        DEBUG("Parse results:\n".Dumper($results));
+        $parser->filer->purge;        
+        return undef;        
+    }
+    
+    DEBUG('Decomposed into $decomposed_to_dir successfully.');        
+    1
+}
+
+
+sub _emit_meta {
+    
+    my $entity = shift;
+    my $meta_options = shift;
+    
+    unless (defined $entity && defined $entity->head) {
+        WARN("Cannot extract meta. Provide meta from error-template.");
+        return _error_meta();                
+    };
+
+    my $head = $entity->head;    
+    my $from = eval { _parse_addresses_str($head->get('From')) };
+    if ( $@ || !(defined $from && @$from)) {
+        ERROR("Invalid email- From: $@ > ". $head->get('From'));
+        return undef;        
+    }
+
+    my $to = eval { _parse_addresses_str($head->get('To')) };
+    if ( $@ || !(defined $to && @$to)) {
+        WARN("Invalid email- To: $@ > ". $head->get('To'));        
+    }
+      
+    
+    my $cc = eval { _parse_addresses_str($head->get('Cc')) };
+    if ( $@) {
+        WARN("Invalid email- Cc: $@ > ". $head->get('Cc'));        
+    }
+            
+    my $bcc = eval {_parse_addresses_str ($head->get('Bcc')) };
+    if ( $@) {
+        WARN("Invalid email- Bcc: $@ > ". $head->get('Bcc'));        
+    }
+
+    my $subject = eval { join(' ', decode_mimewords($head->get('Subject'))) };
+    if ( $@) {
+        WARN("Invalid email subject replaced with default: $@ > " . $head->get('Subject'));        
+        $subject = "(Parsing error... > ". $head->get('Subject')." )";        
+    }
+
+    
+    
+    
+
+    
+    
+    
+
+}
+
+sub _parse_addresses_str {
+    my $str = shift;
+    chomp $str;    
+    return undef unless $str;
+    my @addresses = Email::Address->parse($str);
+    [ map { {Name => $_->name, Address => $_->address  } } @addresses    ]
+}
+
+sub _error_meta_default {
+    my $info = shift;    
+}
 
 
 1;
+
 __END__
 
 =head1 NAME
